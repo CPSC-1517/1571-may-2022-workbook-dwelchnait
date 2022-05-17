753c4c472f972b532b002b5a743ca0fdc347c2f5
Don Welch <dwelch@nait.ca>
2022-05-16 18:52:33 -0600

    behaviours, driver program and static class

--- ReviewSolution/OOPsReview/Program.cs
+++ ReviewSolution/OOPsReview/Program.cs
@@ -1,5 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using OOPsReview.Data;
 
+//Console is a static class
+//you CANNOT create your own instance of a static class
+
 Console.WriteLine("Hello, World!");
+
+//an instance class needs to be created using the new command and the class constructor
+//one needs to declare a variable of datatype Employement
+
+Employment myEmp = new Employment("Level 5 Programer", SupervisoryLevel.Supervisor,15.9); //default contructor
+Console.WriteLine(myEmp.ToString()); //use the instance name to reference items within your class
+
+myEmp.SetEmploymentResponsibilityLevel(SupervisoryLevel.DepartmentHead);
+
+Console.WriteLine(myEmp.ToString());
 
