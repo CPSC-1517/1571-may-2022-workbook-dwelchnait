9904a986ddf0e12dd60b82b6992849536c5af23d
Don Welch <dwelch@nait.ca>
2022-05-18 18:48:47 -0600

    introduction to unit testing, fix errors in some tests

--- ReviewSolution/OOPsReview/Program.cs
+++ ReviewSolution/OOPsReview/Program.cs
@@ -9,10 +9,10 @@ Console.WriteLine("Hello, World!");
 //an instance class needs to be created using the new command and the class constructor
 //one needs to declare a variable of datatype Employement
 
-Employment myEmp = new Employment("Level 5 Programer", SupervisoryLevel.Supervisor,15.9); //default contructor
-Console.WriteLine(myEmp.ToString()); //use the instance name to reference items within your class
+//Employment myEmp = new Employment("Level 5 Programer", SupervisoryLevel.Supervisor,15.9); //default contructor
+//Console.WriteLine(myEmp.ToString()); //use the instance name to reference items within your class
 
-myEmp.SetEmploymentResponsibilityLevel(SupervisoryLevel.DepartmentHead);
+//myEmp.SetEmploymentResponsibilityLevel(SupervisoryLevel.DepartmentHead);
 
-Console.WriteLine(myEmp.ToString());
+//Console.WriteLine(myEmp.ToString());
 
