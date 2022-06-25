09027f37ca37ad15f719b06cd430b53e23c67411
Don Welch <dwelch@nait.ca>
2022-06-24 18:54:43 -0600

    handling multiple buttons on a form, logic on code behind (ModelPage)

--- CSRazorSolution/WebApp/Pages/Samples/BasicDataManagement.cshtml.cs
+++ CSRazorSolution/WebApp/Pages/Samples/BasicDataManagement.cshtml.cs
@@ -15,6 +15,7 @@ namespace WebApp.Pages.Samples
         public int FavouriteCourse { get; set; }
 
         public string Feedback { get; set; }
+        public string ErrorMsg { get; set; }
         public void OnGet()
         {
             if (Num == 0)
@@ -36,6 +37,23 @@ namespace WebApp.Pages.Samples
             else
             {
                 Feedback = $"You entered the value {Num} (display by OnPost)" +
+                $" your mass text is {MassText} and I like the {FavouriteCourse} course.";
+            }
+        }
+
+        public void OnPostSecondButton()
+        {
+            if (Num == 0)
+            {
+                Feedback = "executing the OnPostSecondButton method for a value of zero";
+            }
+            else if(FavouriteCourse == 0)
+            {
+                Feedback ="You did not select a favourite course.";
+            }
+            else
+            {
+                Feedback = $"You entered the value {Num} (display by OnPostSecondButton)" +
                 $" your mass text is {MassText} and I like the {FavouriteCourse} course.";
             }
         }
