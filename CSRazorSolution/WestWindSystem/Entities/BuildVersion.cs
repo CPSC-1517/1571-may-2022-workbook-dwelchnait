b3cf20c47986d403a040c646d079cae9a48cef20
Don Welch <dwelch@nait.ca>
2022-06-27 19:21:04 -0600

    create class lib create first entity BuildVersion

---
+++ CSRazorSolution/WestWindSystem/Entities/BuildVersion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WestWindSystem.Entities
+{
+    public class BuildVersion
+    {
+        //in sql nvarchar(),varchar,nchar,char is declared as a string in your
+        //  Entity definition!!!!!!!!!
+        //sql bit is a bool in C#
+        public int Id { get; set; }
+        public int Major { get; set; }
+        public int Minor { get; set; }
+        public int Build { get; set; }
+        public DateTime ReleaseDate { get; set; }
+
+    }
+}
