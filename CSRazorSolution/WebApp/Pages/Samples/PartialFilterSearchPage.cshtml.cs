4977f9ba9f91e51e5fbefa3c4de0e51ad480b1e2
Don Welch <dwelch@nait.ca>
2022-07-15 15:42:51 -0600

    duplicate filter search for Page() instead of RedirectToPage()

--- CSRazorSolution/WebApp/Pages/Samples/PartialFilterSearchPage.cshtml.cs
+++ CSRazorSolution/WebApp/Pages/Samples/PartialFilterSearchPage.cshtml.cs
@@ -4,19 +4,18 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 #region Additional Namespaces
 using WestWindSystem.BLL;       //this is where the services were coded
 using WestWindSystem.Entities;  //this is where the entity definition is coded
-
 #endregion
 
 
 namespace WebApp.Pages.Samples
 {
-    public class PartialFilterSearchModel : PageModel
+    public class PartialFilterSearchPageModel : PageModel
     {
         #region Private service fields & class constructor
         private readonly TerritoryServices _territoryServices;
         private readonly RegionServices _regionServices;
 
-        public PartialFilterSearchModel(TerritoryServices territoryservices,
+        public PartialFilterSearchPageModel(TerritoryServices territoryservices,
                                         RegionServices regionservices)
         {
             _territoryServices = territoryservices;
@@ -24,15 +23,9 @@ namespace WebApp.Pages.Samples
         }
         #endregion
 
-        [TempData]
         public string Feedback { get; set; }
 
-        //this is bond to the input control via asp-for
-        //this is a two way binding out and in
-        //data is move out and in FOR YOU AUTOMATICALLY
-        //SupportsGet = true will allow this property to be matched to
-        //  a routing parameter of the same name.
-        [BindProperty(SupportsGet = true)]
+        [BindProperty]
         public string searcharg { get; set; }
 
         public List<Territory> TerritoryInfo { get; set; }
@@ -44,17 +37,11 @@ namespace WebApp.Pages.Samples
         [BindProperty]
         public List<Region> RegionList { get; set; } = new();
 
-
         public void OnGet()
         {
             PopulateLists();
-            //check to see if there is a search arg before doing the call to the service
-            if(!string.IsNullOrWhiteSpace(searcharg))
-            {
-                TerritoryInfo = _territoryServices.GetByPartialDescription(searcharg);
-            }
-        }
 
+        }
         public void PopulateLists()
         {
             RegionList = _regionServices.Region_List();
@@ -66,17 +53,11 @@ namespace WebApp.Pages.Samples
             {
                 Feedback = "Required: Search argument is empty.";
             }
-            //the receiving "searcharg" is the routing parameter
-            //the sending "searcharg" is a BindProperty field
-            //the RedirectToPage causes a Get requested to be placed
-            //  on the stack for processing when control is reeturned
-            //  to the browser
-            //Since the processing of this request causes a second
-            //  trip to the server for processing, data needs to be retained
-            //  between the two trips; hence the use of the routing  parameter
-            //This is different then Page() which DOES NOT cause a Get request
-            //  to be placed on the stack.
-            return RedirectToPage(new { searcharg = searcharg });
+            else
+            {
+                TerritoryInfo = _territoryServices.GetByPartialDescription(searcharg);
+            }
+            return Page();
         }
 
         public IActionResult OnPostClear()
@@ -84,7 +65,7 @@ namespace WebApp.Pages.Samples
             Feedback = "";
             searcharg = null;
             ModelState.Clear();
-            return RedirectToPage(new { searcharg = (string?)null });
+            return Page();
         }
 
         public IActionResult OnPostNew()
